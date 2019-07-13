4d49e3a4443b0164eae93dd8b4945844d17c7a96
Shashwat Swain <shashwatswainofficial@gmail.com>
2019-07-13 18:06:06 +0530

    testing basic pygame

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 from pygame.locals import *
 
@@ -13,10 +14,10 @@ class Player:
         self.x = self.x - self.speed
     
     def moveUp(self):
-        self.y = self.y + self.speed
+        self.y = self.y - self.speed
     
     def moveDown(self):
-        self.y = self.y - self.speed
+        self.y = self.y + self.speed
 
 
 class App:
@@ -36,3 +37,52 @@ class App:
                             (self.windowWidth, self.windowHeight),
                             pygame.HWSURFACE)
         pygame.display.set_caption('Reinforcement Snek Game')
+        self._running = True
+        self._image_surf = pygame.image.load('skin.png').convert()
+
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+        
+    def on_loop(self):
+        pass
+    
+    def on_render(self):
+        self._display_surf.fill((0,0,0))
+        self._display_surf.blit(self._image_surf, 
+                            (self.player.x, self.player.y))
+        pygame.display.flip()
+
+    def on_cleanup(self):
+        pygame.quit()
+    
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+        
+        while(self._running):
+            pygame.event.pump()
+            keys = pygame.key.get_pressed()
+
+            if keys[K_RIGHT]:
+                self.player.moveRight()
+            if keys[K_LEFT]:
+                self.player.moveLeft()
+            if keys[K_UP]:
+                self.player.moveUp()
+            if keys[K_DOWN]:
+                self.player.moveDown()
+            if keys[K_ESCAPE]:
+                self._running = False
+
+            self.on_loop()
+            self.on_render()
+        
+        self.on_cleanup()
+
+if __name__=='__main__':
+    
+    snek = App()
+    snek.on_execute()
+            
+            
